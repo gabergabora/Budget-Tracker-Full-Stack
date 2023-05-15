d3f750e054c2348df847ea63a54060cfcbf57d9c
gabergabora <102705568+gabergabora@users.noreply.github.com>
2023-05-16 00:41:38 +0300

    Update User.js

--- server/models/User.js
+++ server/models/User.js
@@ -16,7 +16,7 @@ const userSchema = new mongoose.Schema(
       type: String,
       required: true,
     },
-    hasCreatedAccount: {
+    hasCreatedAccount: {            
       type: Boolean,
       default: false,
     },
